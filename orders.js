db9ae8670d7709083531875f045ebdb2392d0a45
Alex Lamb <alexlamb104@gmail.com>
2022-02-18 10:55:37 -0700

    Completed orders,js

--- orders.js
+++ orders.js
@@ -30,7 +30,18 @@
 */
 
 //CODE HERE
-
+class Ticket {
+    constructor(items, orderTime, customerId){
+        this.items = items
+        this.orderTime = orderTime
+        this.customerId = customerId
+        this.status = 'queued'
+    }
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`The order for ${this.customerId} is now ${newStatus}`)
+    }
+}
 
 
 /*
@@ -45,7 +56,8 @@
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket('pizza, bread, and soda', '7:03 PM', 575)
+console.log(firstTicket)
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +66,5 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking')
+console.log(firstTicket)
